e40506813f223cc7cde39bea233cd1061592e1d2
Narmadha1 <77825068+Narmadha1@users.noreply.github.com>
2024-02-03 16:40:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+let text=document.getElementById("text");
+let heart=document.getElementById("heart");
+const button=document.getElementById("button");
+const heart1=document.querySelector("heart");
+
+button.onclick=function(){
+    text.style.visibility="visible";
+}
+
+
+
+
+
+
+
+
